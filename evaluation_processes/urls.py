194e18390bbd2b8c97650053b020ec48ec829005
yahyaabualhaj <yahya.abualhaj@gmail.com>
2021-06-15 09:54:59 +0300

    part I

--- evaluation_processes/urls.py
+++ evaluation_processes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('evaluation-360-application/', views.evaluation_360_application, name='evaluation_360_application'),
     path('evaluation-360/', views.evaluation_360, name='evaluation_360'),
     path('self-evaluation/', views.self_evaluation, name='self_evaluation'),
+    path('get-evaluetion-application/<int:id>/', views.get_evaluation_application, name='get_evaluation_application'),
+    path('post-evaluetion-application/<int:id>/', views.post_evaluation_application, name='post_evaluation_application'),
 ]
--- evaluation_processes/views.py
+++ evaluation_processes/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
 
-
-# Create your views here.
 
 def home(request):
     return render(request, 'evaluation_processes/home.html')
@@ -25,3 +25,53 @@ def evaluation_360(request):
 
 def self_evaluation(request):
     return render(request, 'evaluation_processes/self_evaluation.html')
+
+
+def get_evaluation_application(request,id):
+    if request.method == 'GET':
+        tabs = [
+            {
+                'name': '',
+                'text': '',
+                'description': '',
+                'active': '',
+                'tabActivated': '',
+                'questions': [],
+
+            }
+        ]
+        questions = {
+            'id': '',
+            'name': '',
+            'value': '',
+            'choices': [],
+            'disabled': False,
+            'required': True,
+        }
+        return JsonResponse({'success': True, 'message': 'The form is submitted successfully'})
+
+
+def post_evaluation_application(request,id):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        data = json.loads(body_unicode)
+        tabs = [
+            {
+                'name': '',
+                'text': '',
+                'description': '',
+                'active': '',
+                'tabActivated': '',
+                'questions': [],
+
+            }
+        ]
+        questions = {
+            'id': '',
+            'name': '',
+            'value': '',
+            'choices': [],
+            'disabled': False,
+            'required': True,
+        }
+        return JsonResponse({'success': True, 'message': 'The form is submitted successfully'})
